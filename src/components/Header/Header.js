6c0d69878a247a0fddeaed4a5ebf670041e59aa1
George Filippakis <geo.filippakis@gmail.com>
2017-11-05 00:44:05 +0200

    styled timer count-down

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,21 +9,56 @@ export default class Header extends React.Component {
         super(props);
         this.state = {
             seconds: 59,
-            timerBtnValue: <span>Start Game <FontAwesome name="hourglass-start" /> </span>,
-            
+            timerBtnValue: 'Start Game',
+            timerBtnStyle: 'primary',
+            timerBtnDisabled: false,
+            icon: 'gamepad',
         }
     }
 
-    
-
     timer = () => {
-        
         setInterval( () => {
-            this.setState(prevState =>({
-            // timerBtnValue: <span> 0:{seconds--} <FontAwesome name="hourglass-start" /> </span>,
-            timerBtnValue: <span>{prevState.seconds} <FontAwesome name="hourglass-start" /> </span>,
-            seconds: prevState.seconds = prevState.seconds - 1,
-            })); 
+            this.setState( prevState => {
+                if (this.state.seconds > 45) {
+                    return {
+                        timerBtnValue: <span>0:{prevState.seconds}  </span>,
+                        seconds: prevState.seconds = prevState.seconds - 1,
+                        icon: 'hourglass-start',
+                        timerBtnStyle: 'primary',                            
+                    };
+                }
+                else if (this.state.seconds > 15) {
+                    return {
+                        timerBtnValue: <span>0:{prevState.seconds}  </span>,
+                        seconds: prevState.seconds = prevState.seconds - 1,
+                        icon: 'hourglass-half',
+                        timerBtnStyle: 'warning',
+                    };
+                }
+                else if (this.state.seconds > 9) {
+                    return {
+                        timerBtnValue: <span>0:{prevState.seconds}  </span>,
+                        seconds: prevState.seconds = prevState.seconds - 1, 
+                        icon: 'hourglass-end',
+                        timerBtnStyle: 'danger',
+                    };
+                }       
+                else if (this.state.seconds > 0) {
+                    return {
+                        timerBtnValue: <span>0:0{prevState.seconds}  </span>,
+                        seconds: prevState.seconds = prevState.seconds - 1, 
+                    };
+                } 
+                else {
+                    return {
+                        timerBtnValue: 'No Time',
+                        seconds: 0, 
+                        icon: '',
+                        timerBtnStyle: 'danger',
+                        timerBtnDisabled: true,
+                    };
+                }      
+        })
     }, 1000)};
 
     render() {
@@ -32,11 +67,13 @@ export default class Header extends React.Component {
                 <Row className="header-row">
                     <h3 className="pull-left custom-header">Play Nine9</h3>
                     <Button
-                        bsStyle="primary"
+                        bsStyle={this.state.timerBtnStyle}
                         className="pull-right header-button"
                         onClick={this.timer}
+                        disabled={this.state.timerBtnDisabled}
                     >
-                        {this.state.timerBtnValue}
+                        
+                        <span>{this.state.timerBtnValue} <FontAwesome name={this.state.icon} /></span>
                     </Button>
                     
 
